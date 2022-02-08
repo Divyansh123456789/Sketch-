67fbbeaf48a1d0225623418512907debfd17cdba
Divyansh <96115798+Divyansh123456789@users.noreply.github.com>
2022-02-08 17:43:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+function setup() {
+    canvas = createCanvas(280,280);
+    canvas.center();
+    background('white');
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis;
+}
+function clearCanvas(){
+    background('white');
+}
+function preload(){
+    classifier = ml5.imageClassifier('DoodleNet');
+}
+function draw(){
+    strokeWeight(13);
+    stroke(0);
+    if(mouseIsPressed){
+        line(mouseX,pmouseY,mouseX,mouseY);
+    }
+}
+function classifyCanvas(){
+    classifier.classify(canvas,gotResults);
+}
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+    document.getElementById("label").innerHTML = "label "+results[0].label;
+
+    document.getElementById("confidence").innerHTML = "confidence "+ Math.round(results[0].confidence * 100) + "%" ;
+
+    UtterThis = new SpeechSynthesisUtterance(results[0].label);
+    synth.speak(UtterThis);
+}
